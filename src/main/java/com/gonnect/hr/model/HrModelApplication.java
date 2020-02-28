61157b352e7a9d66c7694c0fe97517e93a511943
gmalho <gaurav.malhotra@nike.com>
2020-02-28 12:55:36 +0100

    pyro pickle based serializatio

--- src/main/java/com/gonnect/hr/model/HrModelApplication.java
+++ src/main/java/com/gonnect/hr/model/HrModelApplication.java
@@ -3,14 +3,29 @@ package com.gonnect.hr.model;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.pmml4s.model.Model;
+import net.razorvine.pyro.*;
+
+import java.io.IOException;
+
 @SpringBootApplication
 public class HrModelApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Model model = Model.fromFile("/Users/gmalho/Documents/Personal/MyGitHub/hr-model/src/main/python/RandomForestRegressor.pmml");
 
+        var outcome = model.predict(new Double[]{6.5});
+
+        System.out.println(outcome);
+
         System.out.println(model);
+
+
+        NameServerProxy ns = NameServerProxy.locateNS(null);
+        PyroProxy remoteobject = new PyroProxy(ns.lookup("NabeeModel"));
+
+
+        Object output = remoteobject.call("predict",new Double[] {6.5});
 
         SpringApplication.run(HrModelApplication.class, args);
     }
--- src/main/python/random_forest_regression.py
+++ src/main/python/random_forest_regression.py
@@ -1,8 +1,8 @@
 # Random Forest Regression
 
+import matplotlib.pyplot as plt
 # Importing the libraries
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
@@ -24,7 +24,8 @@ y_train = sc_y.fit_transform(y_train)"""
 
 # Fitting Random Forest Regression to the dataset
 from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+
+regressor = RandomForestRegressor(n_estimators=10, random_state=0)
 regressor.fit(X, y)
 
 # Predicting a new result
@@ -35,22 +36,60 @@ from sklearn2pmml.pipeline import PMMLPipeline
 pipeline = PMMLPipeline([
     ("regressor_pmml", regressor)
 ])
+pipeline.fit(X, y)
 
-pipeline.fit(X,y)
+
+# import pickle
+#
+# pickleId = pickle.dump(regressor,open( "model.p", "wb" ))
+
+import Pyro4
+
+# add pickle serializer to pyro
+# export PYRO_SERIALIZERS_ACCEPTED=serpent,json,marshal,pickle,dill
+# start name server pyro4-ns
+daemon = Pyro4.Daemon()
+
+import numpy
+@Pyro4.expose
+class NabeeModel(RandomForestRegressor):
+    def __init__(self, obj):
+        self._wrapped_obj = obj
+    def __getattr__(self, attr):
+        if attr in self.__dict__:
+            return getattr(self, attr)
+        return getattr(self._wrapped_obj, attr)
+    def predict(self, X):
+        print(X)
+        value = numpy.array(X)
+        value = value.reshape(1, -1)
+        print(value)
+        output = self._wrapped_obj.predict(value)
+        print('output: ', output)
+        return  output.tolist()
+
+nabeeModel = NabeeModel(regressor)
+nabeeModel.fit(X,y)
+nabeeModel.predict([[6.1]])
+
+daemon = daemon.serveSimple({nabeeModel: "NabeeModel"})
+
+print(daemon)
+
+daemon.requestLoop()
 
 
 from sklearn2pmml import sklearn2pmml
 
-sklearn2pmml(pipeline, "RandomForestRegressor.pmml", with_repr = True)
-
+sklearn2pmml(pipeline, "RandomForestRegressor.pmml", with_repr=True)
 
 print("++++++", y_pred)
 
 # Visualising the Random Forest Regression results (higher resolution)
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
 plt.title('Truth or Bluff (Random Forest Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
