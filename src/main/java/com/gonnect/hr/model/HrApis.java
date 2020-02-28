2b89801961e73de6d48e7ad7371435d7d821608c
gmalho <gaurav.malhotra@nike.com>
2020-02-28 16:44:07 +0100

    code cleanup

--- src/main/java/com/gonnect/hr/model/HrApis.java
+++ src/main/java/com/gonnect/hr/model/HrApis.java
@@ -2,6 +2,7 @@ package com.gonnect.hr.model;
 
 import net.razorvine.pyro.NameServerProxy;
 import net.razorvine.pyro.PyroProxy;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,21 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.util.List;
 
-@RestController
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@RestController
 public class HrApis {
 
-    private final PyroProxy remoteobject;
+    private final PyroProxy remoteObject;
 
     public HrApis() throws IOException {
         NameServerProxy ns = NameServerProxy.locateNS(null);
-        remoteobject = new PyroProxy(ns.lookup("NabeeModel"));
+        remoteObject = new PyroProxy(ns.lookup("NabeeModel"));
     }
 
 
-    @GetMapping("/hr/predict")
+    @GetMapping(value = "/hr/predict", produces = APPLICATION_JSON_VALUE)
     public ModelPredict predict(@RequestParam double level) throws IOException {
-        List<Double> output = (List<Double>) remoteobject.call("predict",new Double[] {level});
+        List<Double> output = (List<Double>) remoteObject.call("predict", new Double[]{level});
 
         return new ModelPredict(output.get(0));
     }
