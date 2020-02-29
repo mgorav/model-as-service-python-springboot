5b68aa2606f8ad2014b5cee2424ad77307f5893f
gmalho <gaurav.malhotra@nike.com>
2020-02-29 11:16:59 +0100

    added Hazelcast based distrubuted caching for Model

--- src/test/java/com/gonnect/hr/model/HrModelApplicationTests.java
+++ src/test/java/com/gonnect/hr/model/HrModelApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HrModelApplicationTests {
 
+    // Make sure Spring context is UP and running
     @Test
     void contextLoads() {
     }
